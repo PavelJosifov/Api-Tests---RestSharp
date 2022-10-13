58e4b33674ac81469345c6679a2a1d88b7efe996
Pavel Josifov <josifov.pavel@gmail.com>
2022-10-13 16:53:48 +0300

    Additional tests.

---
+++ ZippopotamusApiTests.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+namespace URL_tests
+{
+    public class ZippopotamusApiTests
+    {
+        private const string BaseUrl = "https://api.zippopotam.us";
+        private RestClient client;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            this.client = new RestClient(BaseUrl);
+            this.client.Options.Timeout = 3000;
+
+        }
+        [Test]
+        public void Test_Zippopotamus_Responses()
+        {
+            //arrange
+            var request = new RestRequest("/bg/7000", Method.Get);
+
+            //act
+            var response = client.Execute(request);
+
+            //assert 
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+            var ActualResult = System.Text.Json.JsonSerializer.Deserialize<ZippopotamusResponse>(response.Content);
+            var places = new Place[]
+
+            {
+               new Place("Русе / Ruse","25.9708","Русе / Ruse","RSE","43.8564" )
+
+            };
+            var expectedResponse = new ZippopotamusResponse("7000", "Bulgaria", "BG", places);
+
+
+
+            AssertEqualObjects(expectedResponse, ActualResult);
+
+        }
+
+        private void AssertEqualObjects(object obj1, object obj2)
+        {
+            string obj1JSON = System.Text.Json.JsonSerializer.Serialize(obj1);
+            string obj2JSON = System.Text.Json.JsonSerializer.Serialize(obj2);
+            Assert.AreEqual(obj1JSON, obj2JSON);
+        }
+    }
+}
---
+++ ZippopotamusResponse.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace URL_tests
+{
+
+    internal class ZippopotamusResponse
+    {
+        public ZippopotamusResponse(string postcode, string country, string countryabbreviation, Place[]places)
+        {
+            this.PostCode = postcode;
+            this.Country = country;
+            this.CountryAbbreviation = countryabbreviation;
+            this.Places = places;
+        }
+        public ZippopotamusResponse()
+        {
+
+        }
+
+        [JsonPropertyName("post code")]
+        public string PostCode { get; set; }
+        [JsonPropertyName("country")]
+        public string Country { get; set; }
+        [JsonPropertyName("country abbreviation")]
+        public string CountryAbbreviation { get; set; }
+        [JsonPropertyName("places")]
+        public Place[] Places { get; set; }
+
+
+    }
+    public class Place
+    {
+        public Place(string placename, string longtitude, string state, string stateabbreviation, string latitude)
+        {
+            this.PlaceName = placename;
+            this.Longitude = longtitude;
+            this.State = state;
+            this.StateAbbreviation = stateabbreviation;
+            this.Latitude = latitude;
+        }
+        public Place()
+        {
+
+        }
+        [JsonPropertyName("place name")]
+        public string PlaceName { get; set; }
+        [JsonPropertyName("longitude")]
+        public string Longitude { get; set; }
+        [JsonPropertyName("state")]
+        public string State { get; set; }
+        [JsonPropertyName("state abbreviation")]
+        public string StateAbbreviation { get; set; }
+        [JsonPropertyName("latitude")]
+        public string Latitude { get; set; }
+
+    }
+}
